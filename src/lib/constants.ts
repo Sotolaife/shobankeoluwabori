a46cffee1716492810d89257c01a31a8372c6435
Ifeoluwa Sotola <sotolaifeoluwa@gmail.com>
2025-11-05 10:27:26 +0000

    This is his email, integrate it. Pavillionartworld@gmail.com

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const WHATSAPP_NUMBER = "2348160930769";
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}`;
-export const CONTACT_EMAIL = "pavillion.art@example.com";
+export const CONTACT_EMAIL = "Pavillionartworld@gmail.com";
 export const INSTAGRAM_HANDLE = "pavillion_art";
 export const INSTAGRAM_LINK = `https://instagram.com/${INSTAGRAM_HANDLE}`;
 export const TIKTOK_HANDLE = "pavillion_art";
